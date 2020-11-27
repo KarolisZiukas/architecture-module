e11e2fae5717df933e2ae3862fdac3eb3e71a36c
Karolis Ziukas <karolis.ziukas@nordsec.com>
2020-11-27 22:56:40 +0200

    Test

--- src/main/java/com/example/architecture_module/database/Converters.kt
+++ src/main/java/com/example/architecture_module/database/Converters.kt
@@ -7,7 +7,6 @@ class Converters {
     @TypeConverter
     fun listToJson(value: List<String>?) = Gson().toJson(value)
 
-
     @TypeConverter
     fun jsonToList(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
 }
